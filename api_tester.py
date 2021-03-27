58d8aa77c57dd0d8bdbd51ca4118c8a7709764cf
JB-Tellez <jb.tellez@gmail.com>
2021-03-26 17:16:43 -0700

    fixes api_tester typo

--- api_tester.py
+++ api_tester.py
@@ -70,7 +70,7 @@ class ApiTester:
         return response.json()
 
     # TODO adjust parameter names to match API
-    def create(self, location, description=None, owner=None):
+    def create(self, name, description=None, owner=None):
         """creates a resource in api
 
         Usage:
@@ -89,7 +89,7 @@ class ApiTester:
         }
 
         data = {
-            "location": location,
+            "name": name,
             "description": description,
             "owner": owner,
         }
