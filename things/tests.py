6ed423cf037ee0120bca41c41807d9564860e8c7
JB-Tellez <jb.tellez@gmail.com>
2022-03-11 01:09:17 -0800

    adds failing placeholder test

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class ThingsTests(TestCase):
+    # TODO: test your app
+    def test_your_app(self):
+        self.assertEqual("I have many tests", "I have no tests")
