fd9c235427dd8e61316f876129a95dfabb005f75
JB Tellez <jb.tellez@gmail.com>
2022-09-15 16:09:53 -0700

    adds token handling enhancements - time and payload (#3)

---
+++ project/views.py
@@ -0,0 +1,19 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token["email"] = user.email
+        token["username"] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
