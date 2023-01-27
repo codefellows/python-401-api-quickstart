b29c2c75e99c7549f43564611e6785b8bf9fc6e4
JB Tellez <jb.tellez@gmail.com>
2023-01-27 13:08:25 -0800

    adds about page and style

--- project/urls.py
+++ project/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     ),
     path("things/", include("things.urls_front")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("about/", TemplateView.as_view(template_name="about.html"), name="about"),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
