f629c0a0213a9ec1ea17d812f3f82544acace90a
JB Tellez <jb.tellez@gmail.com>
2023-01-30 13:24:23 -0800

    adds vercel config

--- project/wsgi.py
+++ project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+# vercel deployment requires "app"
+app = application
